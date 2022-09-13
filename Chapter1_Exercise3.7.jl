45c41d77aea9faa94042499a2db2de6e73655856
acebedomarkace <acebedo.mark@gmail.com>
2022-09-13 21:48:00 +0800

    Adding 3.7 for Chapter 1
    This is just a single item

---
+++ Chapter1_Exercise3.7.jl
@@ -0,0 +1,113 @@
+### A Pluto.jl notebook ###
+# v0.19.11
+
+using Markdown
+using InteractiveUtils
+
+# This Pluto notebook uses @bind for interactivity. When running this notebook outside of Pluto, the following 'mock version' of @bind gives bound variables a default value (instead of an error).
+macro bind(def, element)
+    quote
+        local iv = try Base.loaded_modules[Base.PkgId(Base.UUID("6e696c72-6542-2067-7265-42206c756150"), "AbstractPlutoDingetjes")].Bonds.initial_value catch; b -> missing; end
+        local el = $(esc(element))
+        global $(esc(def)) = Core.applicable(Base.get, el) ? Base.get(el) : iv(el)
+        el
+    end
+end
+
+# ╔═╡ 095c5690-2829-11ed-0d49-af3b33334ea7
+md"""
+# Numbers to 10,000
+"""
+
+# ╔═╡ c1ba96d8-4f88-4338-ab8c-e7b399f63115
+md"""
+### Exercise 3
+"""
+
+# ╔═╡ d7b50aee-51eb-4089-98aa-30d804485faa
+md""" #### 7. 
+What is the smallest 4-digit number that you can make using all the digits **7**, **5**, **2**, **6** ?
+
+
+$(@bind a1 html"<input type=text >")
+
+"""
+
+# ╔═╡ 760ac96e-2a25-4766-b3f9-ca3eacd86206
+ans_a1 = (a1);
+
+# ╔═╡ f2d7328a-ec9b-4f7d-b388-2fefd02e9e61
+begin
+	function checker_a1(ans)
+		if ("2567")==ans
+			print("👍 That's correct!")
+			return 1
+		elseif ("")==ans
+			print("😀 Waiting for your answer")
+			return ""
+		else
+			print("🤔 Something is not right")
+			return 0
+		end
+	end
+end;
+
+# ╔═╡ 62006327-88e3-43e4-a4be-326b73a9d278
+result_a1=checker_a1(ans_a1);
+
+# ╔═╡ 1d5a31d2-3374-4a47-9c17-d065ccf34ca8
+md""" # Summary
+"""
+
+# ╔═╡ ced0a305-0ce5-4490-8b63-7074a530ba1d
+begin
+	results = [result_a1]
+
+	top_score = length(results)
+	
+	counter=0
+	for result in results
+		if result == 1
+			counter += 1
+		end
+	end
+
+	
+	
+	if top_score==counter
+		print("🏆 Perfect score! $(top_score) out of $(top_score)!")
+	elseif 0 == counter
+		exit
+	elseif top_score > counter
+		print("Out of $(top_score) Items, you got: ")
+		print(counter)
+		print(" correctly")
+	end
+end;
+
+# ╔═╡ 00000000-0000-0000-0000-000000000001
+PLUTO_PROJECT_TOML_CONTENTS = """
+[deps]
+"""
+
+# ╔═╡ 00000000-0000-0000-0000-000000000002
+PLUTO_MANIFEST_TOML_CONTENTS = """
+# This file is machine-generated - editing it directly is not advised
+
+julia_version = "1.7.3"
+manifest_format = "2.0"
+
+[deps]
+"""
+
+# ╔═╡ Cell order:
+# ╟─095c5690-2829-11ed-0d49-af3b33334ea7
+# ╟─c1ba96d8-4f88-4338-ab8c-e7b399f63115
+# ╟─d7b50aee-51eb-4089-98aa-30d804485faa
+# ╟─62006327-88e3-43e4-a4be-326b73a9d278
+# ╟─760ac96e-2a25-4766-b3f9-ca3eacd86206
+# ╟─f2d7328a-ec9b-4f7d-b388-2fefd02e9e61
+# ╟─1d5a31d2-3374-4a47-9c17-d065ccf34ca8
+# ╟─ced0a305-0ce5-4490-8b63-7074a530ba1d
+# ╟─00000000-0000-0000-0000-000000000001
+# ╟─00000000-0000-0000-0000-000000000002
