12c4b32f965d9dcc38cef28650f9fffb516e1490
acebedomarkace <acebedo.mark@gmail.com>
2022-09-19 16:57:26 +0800

    Update Chapter1_Exercise4.7.jl

--- Chapter1_Exercise4.7.jl
+++ Chapter1_Exercise4.7.jl
@@ -36,17 +36,17 @@ md"""
 md"""
 |  |  |  |  |  |
 --- | --- | ---| ---| ---|
-|4024|4034|$(@bind a1 html"<input type=text size=2>")|$(@bind a2 html"<input type=text size=2>")|4064|
+|4024|4034|$(@bind a1 html"<input type=text size=3>")|$(@bind a2 html"<input type=text size=3>")|4064|
 ||3934||
 ||3834||5636||
-||$(@bind a3 html"<input type=text size=2>")||$(@bind a4 html"<input type=text size=2>")||
-||$(@bind a5 html"<input type=text size=2>")|4634|5634|$(@bind a6 html"<input type=text size=2>")|
-||||$(@bind a7 html"<input type=text size=2>")||
-||||$(@bind a8 html"<input type=text size=2>")||
-|2631|$(@bind a9 html"<input type=text size=2>")|4631|$(@bind a10 html"<input type=text size=2>")|$(@bind a11 html"<input type=text size=2>")|
+||$(@bind a3 html"<input type=text size=3>")||$(@bind a4 html"<input type=text size=3>")||
+||$(@bind a5 html"<input type=text size=3>")|4634|5634|$(@bind a6 html"<input type=text size=3>")|
+||||$(@bind a7 html"<input type=text size=3>")||
+||||$(@bind a8 html"<input type=text size=3>")||
+|2631|$(@bind a9 html"<input type=text size=3>")|4631|$(@bind a10 html"<input type=text size=3>")|$(@bind a11 html"<input type=text size=3>")|
 |2630|||5630|
-|$(@bind a12 html"<input type=text size=2>")||||
-|$(@bind a13 html"<input type=text size=2>")|2728|$(@bind a14 html"<input type=text size=2>")|$(@bind a15 html"<input type=text size=2>")|3028
+|$(@bind a12 html"<input type=text size=3>")||||
+|$(@bind a13 html"<input type=text size=3")|2728|$(@bind a14 html"<input type=text size=3>")|$(@bind a15 html"<input type=text size=3>")|3028
 """
 
 # ╔═╡ 760ac96e-2a25-4766-b3f9-ca3eacd86206
@@ -115,9 +115,8 @@ PlutoUI = "~0.7.40"
 PLUTO_MANIFEST_TOML_CONTENTS = """
 # This file is machine-generated - editing it directly is not advised
 
-julia_version = "1.8.0"
+julia_version = "1.7.3"
 manifest_format = "2.0"
-project_hash = "84a83347806aa0c59249a2308e3c5683a3eab219"
 
 [[deps.AbstractPlutoDingetjes]]
 deps = ["Pkg"]
@@ -127,7 +126,6 @@ version = "1.1.4"
 
 [[deps.ArgTools]]
 uuid = "0dad84c5-d112-42e6-8d28-ef12dabb789f"
-version = "1.1.1"
 
 [[deps.Artifacts]]
 uuid = "56f22d72-fd6d-98f1-02f0-08ddc0907c33"
@@ -144,7 +142,6 @@ version = "0.11.4"
 [[deps.CompilerSupportLibraries_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "e66e0078-7015-5450-92f7-15fbd957f2ae"
-version = "0.5.2+0"
 
 [[deps.Dates]]
 deps = ["Printf"]
@@ -153,7 +150,6 @@ uuid = "ade2ca70-3891-5945-98fb-dc099432e06a"
 [[deps.Downloads]]
 deps = ["ArgTools", "FileWatching", "LibCURL", "NetworkOptions"]
 uuid = "f43a241f-c20a-4ad4-852c-f6b1247861c6"
-version = "1.6.0"
 
 [[deps.FileWatching]]
 uuid = "7b1f6079-737a-58dc-b8bc-7a2ca5c1b5ee"
@@ -195,12 +191,10 @@ version = "0.21.3"
 [[deps.LibCURL]]
 deps = ["LibCURL_jll", "MozillaCACerts_jll"]
 uuid = "b27032c2-a3e7-50c8-80cd-2d36dbcbfd21"
-version = "0.6.3"
 
 [[deps.LibCURL_jll]]
 deps = ["Artifacts", "LibSSH2_jll", "Libdl", "MbedTLS_jll", "Zlib_jll", "nghttp2_jll"]
 uuid = "deac9b47-8bc7-5906-a0fe-35ac56dc84c0"
-version = "7.84.0+0"
 
 [[deps.LibGit2]]
 deps = ["Base64", "NetworkOptions", "Printf", "SHA"]
@@ -209,7 +203,6 @@ uuid = "76f85450-5226-5b5a-8eaa-529ad045b433"
 [[deps.LibSSH2_jll]]
 deps = ["Artifacts", "Libdl", "MbedTLS_jll"]
 uuid = "29816b5a-b9ab-546f-933c-edad1886dfa8"
-version = "1.10.2+0"
 
 [[deps.Libdl]]
 uuid = "8f399da3-3557-5675-b5ff-fb832c97cbdb"
@@ -228,23 +221,19 @@ uuid = "d6f4376e-aef5-505a-96c1-9c027394607a"
 [[deps.MbedTLS_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "c8ffd9c3-330d-5841-b78e-0817d7145fa1"
-version = "2.28.0+0"
 
 [[deps.Mmap]]
 uuid = "a63ad114-7e13-5084-954f-fe012c677804"
 
 [[deps.MozillaCACerts_jll]]
 uuid = "14a3606d-f60d-562e-9121-12d972cd8159"
-version = "2022.2.1"
 
 [[deps.NetworkOptions]]
 uuid = "ca575930-c2e3-43a9-ace4-1e988b2c1908"
-version = "1.2.0"
 
 [[deps.OpenBLAS_jll]]
 deps = ["Artifacts", "CompilerSupportLibraries_jll", "Libdl"]
 uuid = "4536629a-c528-5b80-bd46-f80d51c5b363"
-version = "0.3.20+0"
 
 [[deps.Parsers]]
 deps = ["Dates"]
@@ -255,7 +244,6 @@ version = "2.4.0"
 [[deps.Pkg]]
 deps = ["Artifacts", "Dates", "Downloads", "LibGit2", "Libdl", "Logging", "Markdown", "Printf", "REPL", "Random", "SHA", "Serialization", "TOML", "Tar", "UUIDs", "p7zip_jll"]
 uuid = "44cfe95a-1eb2-52ea-b672-e2afdf69b78f"
-version = "1.8.0"
 
 [[deps.PlutoUI]]
 deps = ["AbstractPlutoDingetjes", "Base64", "ColorTypes", "Dates", "Hyperscript", "HypertextLiteral", "IOCapture", "InteractiveUtils", "JSON", "Logging", "Markdown", "Random", "Reexport", "UUIDs"]
@@ -282,7 +270,6 @@ version = "1.2.2"
 
 [[deps.SHA]]
 uuid = "ea8e919c-243c-51af-8825-aaa63cd721ce"
-version = "0.7.0"
 
 [[deps.Serialization]]
 uuid = "9e88b42a-f829-5b0c-bbe9-9e923198166b"
@@ -301,12 +288,10 @@ uuid = "10745b16-79ce-11e8-11f9-7d13ad32a3b2"
 [[deps.TOML]]
 deps = ["Dates"]
 uuid = "fa267f1f-6049-4f14-aa54-33bafae1ed76"
-version = "1.0.0"
 
 [[deps.Tar]]
 deps = ["ArgTools", "SHA"]
 uuid = "a4e569a6-e804-4fa4-b0f3-eef7a1d5b13e"
-version = "1.10.0"
 
 [[deps.Test]]
 deps = ["InteractiveUtils", "Logging", "Random", "Serialization"]
@@ -327,22 +312,18 @@ uuid = "4ec0a83e-493e-50e2-b9ac-8f72acf5a8f5"
 [[deps.Zlib_jll]]
 deps = ["Libdl"]
 uuid = "83775a58-1f1d-513f-b197-d71354ab007a"
-version = "1.2.12+3"
 
 [[deps.libblastrampoline_jll]]
 deps = ["Artifacts", "Libdl", "OpenBLAS_jll"]
 uuid = "8e850b90-86db-534c-a0d3-1478176c7d93"
-version = "5.1.1+0"
 
 [[deps.nghttp2_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "8e850ede-7688-5339-a07c-302acd2aaf8d"
-version = "1.48.0+0"
 
 [[deps.p7zip_jll]]
 deps = ["Artifacts", "Libdl"]
 uuid = "3f19e933-33d8-53b3-aaab-bd5110c3b7a0"
-version = "17.4.0+0"
 """
 
 # ╔═╡ Cell order:
@@ -350,7 +331,7 @@ version = "17.4.0+0"
 # ╟─095c5690-2829-11ed-0d49-af3b33334ea7
 # ╟─c1ba96d8-4f88-4338-ab8c-e7b399f63115
 # ╟─95567e32-d284-4ecd-bdc8-6761cff0c075
-# ╟─6e971be6-c1ce-46fe-9b0a-4399316ef1a3
+# ╠═6e971be6-c1ce-46fe-9b0a-4399316ef1a3
 # ╟─62006327-88e3-43e4-a4be-326b73a9d278
 # ╟─760ac96e-2a25-4766-b3f9-ca3eacd86206
 # ╟─f2d7328a-ec9b-4f7d-b388-2fefd02e9e61
